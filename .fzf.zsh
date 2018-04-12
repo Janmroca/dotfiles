e5a8fcadc203ce4c62ac773f44a3d2c820b1d9d8
Jan Manrique <jan.manrique@est.fib.upc.edu>
2018-04-12 08:18:16 +0200

    Some changes

--- .fzf.zsh
+++ .fzf.zsh
@@ -14,7 +14,7 @@ source "/home/jan/.fzf/shell/key-bindings.zsh"
 
 # Configuration
 # -------------
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='
