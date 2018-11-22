f5566425967f0c96d09c8f6c53a7208a2a96ab30
Jan Manrique <jmanrique@quercus.biz>
2018-11-22 14:00:35 +0100

    Updated fzf, added pv

--- .fzf.zsh
+++ .fzf.zsh
@@ -14,10 +14,9 @@ source "/home/jan/.fzf/shell/key-bindings.zsh"
 
 # Configuration
 # -------------
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS=""
 export FZF_DEFAULT_OPTS='
+  --bind="ctrl-o:execute(nvim {})+abort"
   --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
   --color info:108,prompt:109,spinner:108,pointer:168,marker:168
 '
